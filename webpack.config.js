0dca5e68231bb0693df1bcbc91d7eba28ada069a
pyrus-vagus <grushadaria@gmail.com>
2022-01-13 20:37:25 +0300

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
 var path = require("path");
 var webpack = require("webpack");
 
-module.exports = function(env) {
+module.exports = function (env) {
+  var pack = require("./package.json");
 
-	var pack = require("./package.json");
-	var babelSettings = {
-		extends: path.join(__dirname, '/.babelrc')
-	};
+  var babelSettings = {
+    extends: path.join(__dirname, "/.babelrc"),
+  };
 
-	var config = {
-		mode: "development",
-		entry: {
-			"app" : "./sources/app.js"
-		},
-		output: {
-			path: path.join(__dirname, "codebase"),
-			publicPath:"/codebase/",
-			filename: "[name].js",
-			chunkFilename: "[name].bundle.js"
-		},
-		devtool: "inline-source-map",
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					loader: "babel-loader?" + JSON.stringify(babelSettings)
-				}
-			]
-		},
-		resolve: {
-			extensions: [".js"],
-			modules: ["./sources", "node_modules"],
-			alias:{
-				//"webix-jet":path.resolve(__dirname, "../webix-jet/dist/"),
-				"jet-views":path.resolve(__dirname, "sources/views"),
-				"jet-locales":path.resolve(__dirname, "sources/locales")
-			}
-		},
-		//next config is used only by router-url sample
-		devServer:{
-			historyApiFallback:{
-				index : "index.html"
-			}
-		}
-	};
+  var config = {
+    mode: "development",
+    entry: {
+      app: "./sources/app.js",
+    },
+    output: {
+      path: path.join(__dirname, "codebase"),
+      publicPath: "/codebase/",
+      filename: "[name].js",
+      chunkFilename: "[name].bundle.js",
+    },
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader?" + JSON.stringify(babelSettings),
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js"],
+      modules: ["./sources", "node_modules"],
+      alias: {
+        "jet-views": path.resolve(__dirname, "sources/views"),
+        "jet-locales": path.resolve(__dirname, "sources/locales"),
+      },
+    },
+    devServer: {
+      client: {
+        logging: "error",
+      },
+      static: __dirname,
+      historyApiFallback: true,
+    },
+  };
 
-	return config;
-}
+  return config;
+};
